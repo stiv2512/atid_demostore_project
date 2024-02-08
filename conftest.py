74c25a9773feda578ea9ca56630ff3075252d84e
stiv2512 <stiv2512@yandex.ru>
2024-02-08 19:10:11 +0200

    Add options lang and headless to parser

--- conftest.py
+++ conftest.py
@@ -1,9 +1,22 @@
 import pytest
+from pytest import Parser
+from pytest import FixtureRequest
 from selenium import webdriver
 
+def pytest_addoption(parser: Parser):
+    parser.addoption('--headless', action='store_true', help = 'Run browser in headless mode')
+    parser.addoption('--lang', action='store', default='en-US', help='Define the language of the browser')
+
 @pytest.fixture(scope='function')
-def browser():
-    browser = webdriver.Chrome()
+def browser(request: FixtureRequest):
+    options = webdriver.ChromeOptions()
+    headless = request.config.getoption('headless')
+    if headless:
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+    lang = request.config.getoption('lang')
+    options.add_argument(f'--lang={lang}')
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
 
