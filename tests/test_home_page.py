9c786552479ea5cd8c87bd2b793fd336b654e864
stiv2512 <stiv2512@yandex.ru>
2024-02-08 20:50:41 +0200

    modified test

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,12 +1,21 @@
 from pages.home_page import HomePage
 import pytest
 
-url = 'https://atid.store/'
 
-@pytest.mark.smoke
+
 @pytest.mark.home
-def test_user_can_access_to_home_page(browser):
-    home_page = HomePage(browser, url)
-    home_page.open()
-    home_page.should_be_home_page()
+class TestHomePage:
+
+    url = 'https://atid.store/'
+    
+    def setup_method(self, browser):
+        self.home_page = HomePage(browser, self.url)
+        self.home_page.open()
+    
+    @pytest.mark.smoke
+    def test_user_can_access_to_home_page(self):
+        self.home_page.should_be_home_page()
+
+    def test_user_can_click_store_button(self):
+        pass
     
