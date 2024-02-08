fa65ecd081d79215c2f25f370fc8f58e1c6e2a12
stiv2512 <stiv2512@yandex.ru>
2024-02-08 19:40:45 +0200

    add methods structure

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,3 +7,29 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    # PRESENCE OF ELEMENTS
+    def element_is_present(self):
+        pass
+
+    def element_is_visible(self):
+        pass
+
+    def element_is_not_present(self):
+        pass
+
+    def element_is_not_visible(self):
+        pass
+
+    # CLICKS
+
+    def click_element(self):
+        pass
+
+    def click_after_scroll(self):
+        pass
+
+    #Reading function
+
+    def get_visible_text(self):
+        pass
