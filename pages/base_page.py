4f6e6e7341c90d951675af3c36f57d6ab3a859ee
stiv2512 <stiv2512@yandex.ru>
2024-02-08 19:50:32 +0200

    ad presence of elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class BasePage:
     def __init__(self, browser: Chrome, url: str):
@@ -12,14 +15,30 @@ class BasePage:
     def element_is_present(self):
         pass
 
-    def element_is_visible(self):
-        pass
+    def element_is_visible(self, locator: tuple, waiting=5) -> bool:
+        try:
+            WebDriverWait(self.browser, waiting).until(
+                EC.visibility_of_element_located(locator)
+            )
+            return True
+        except TimeoutException:
+            return False
+
 
     def element_is_not_present(self):
         pass
 
-    def element_is_not_visible(self):
-        pass
+    def element_is_not_visible(self, locator: tuple, waiting=5) -> bool:
+        try:
+            WebDriverWait(self.browser, waiting).until(
+                EC.invisibility_of_element_located(locator)
+            )
+            return True
+        except TimeoutException:
+            return False
+    
+    def element_disappeared(self, locator: tuple, waiting=5):
+        return not self.element_is_visible(locator, waiting)
 
     # CLICKS
 
