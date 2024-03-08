6c5786f14b4ba73e2ffea320f820d2c46dc8e163
stiv2512 <146644309+stiv2512@users.noreply.github.com>
2024-03-08 22:49:16 +0200

    Listed necessary functions for navigation through home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,11 +3,15 @@ from ._locators import HomePageLocators
 
 class HomePage(BasePage):
     expected_text = 'ATID Demo Store'
-    def should_be_homepage(self):
-        assert self.element_is_visible(HomePageLocators.MAIN_TITLE), "Main title is not visible"
-        text = self.get_visible_text(HomePageLocators.MAIN_TITLE)
-        assert text == self.expected_text, 'Not expected text of main title'
+    def should_be_home_page(self):
+        assert self.element_is_visible(HomePageLocators.WEBSITE_TITLE), "'Home' page title is not visible, that may be not ATID Store"
+        text = self.get_visible_text(HomePageLocators.WEBSITE_TITLE)
+        assert text == self.expected_text, "'Home' page title is not as expected" #f expected = {}, in reality = {}
 
+    def click_cart_button(self):
+        self.click_element(HomePageLocators.CART_BUTTON)
+    
     def click_store_button(self):
-        self.click_element(HomePageLocators.STORE_BUTTON)
+        self.click_element(HomePageLocators.STORE_BUTTON) 
+
         
