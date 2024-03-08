d097939dbb3ceab02227de27aff49eef9de74503
stiv2512 <146644309+stiv2512@users.noreply.github.com>
2024-03-08 22:46:19 +0200

    Added locators in _locators.py to all tested pages

--- pages/_locators.py
+++ pages/_locators.py
@@ -1,9 +1,24 @@
 from selenium.webdriver.common.by import By
 
 class HomePageLocators:
-    MAIN_TITLE = (By.CSS_SELECTOR, ".elementor-widget-container h1")
-    STORE_BUTTON = (By.CSS_SELECTOR, "#menu-item-45 a")
+    WEBSITE_TITLE = (By.CSS_SELECTOR, 'style ~ .elementor-heading-title')
+    CART_BUTTON = (By.CSS_SELECTOR, '.ast-cart-menu-wrap') #'div > .cart-container') #.ast-cart-menu-wrap
+    STORE_BUTTON = (By.CSS_SELECTOR, '#menu-item-45 > a')
 
 class StorePageLocators:
-    CATEGORIES_LIST = (By.CSS_SELECTOR, ".product-categories li a")
+    FILTER_BY_PRICE = (By.CSS_SELECTOR, '#woocommerce_price_filter-2 > .widget-title')
+    ITEM_LINK = (By.CSS_SELECTOR, '#woocommerce_top_rated_products-1 > ul > li:nth-child(1) > a')
+
+class ItemPageLocators:
+    ITEM_TITLE = (By.CSS_SELECTOR, 'h1.product_title')
+    ADD_TO_CART = (By.CSS_SELECTOR, '.single_add_to_cart_button')
+
+class CartPageLocators:
+    CART_TITLE = (By.CSS_SELECTOR, '.elementor-heading-title.elementor-size-default')
+    ITEM_IN_CART = (By.CSS_SELECTOR, 'td.product-name > a')
+    ITEM_NUMBER = (By.CSS_SELECTOR, "[type='number']")
+    UPDATE_CART = (By.CSS_SELECTOR, 'div ~ button')
+    SUBTOTAL_PRICE = (By.CSS_SELECTOR, 'td.product-subtotal > span > bdi')
+    DELETE_BUTTON = (By.CSS_SELECTOR, 'td .remove')
+    UNDO = (By.CSS_SELECTOR, 'div.woocommerce-message > a')
 
