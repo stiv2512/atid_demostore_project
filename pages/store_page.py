23509829ee5b9024f941c08c3bf33251c759c368
stiv2512 <146644309+stiv2512@users.noreply.github.com>
2024-03-08 22:51:02 +0200

    Enlarged class on store_page.py

--- pages/store_page.py
+++ pages/store_page.py
@@ -2,6 +2,10 @@ from .base_page import BasePage
 from ._locators import StorePageLocators
 
 class StorePage(BasePage):
-    def should_be_store_page(self):
-        categories_list = self.get_list_of_elements(StorePageLocators)
-        assert len(categories_list) == 3, f'There is not enough categories on the page, found {len(categories_list)}'
+    def should_be_store_page(self, expected_text):
+        assert self.element_is_visible(StorePageLocators.FILTER_BY_PRICE), "'Filter by Price' heading does not exist, this may not be a 'Home' page"
+        text = self.get_visible_text(StorePageLocators.FILTER_BY_PRICE)
+        assert text == expected_text, f'Expected {expected_text}, got {text}'
+    
+    def click_item_link(self):
+        self.click_element(StorePageLocators.ITEM_LINK)
